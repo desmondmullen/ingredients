37c4fa6d92edaefd152edc1cbbc6449e3cb03016
Desmond Mullen <dsm@busylittlestudios.com>
2019-05-30 11:55:27 -0400

    basic function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,33 +11,82 @@ class Home extends Component {
     };
 
     queryUSDA = () => {
-        document.getElementById('result').innerText = 'searching...'
+        document.getElementById('result').innerText = 'searching...';
         const theQuery = document.getElementById('query').value;
+        const theAlerts = ((document.getElementById('alert').value).toUpperCase()).split(' ');
         API.queryUSDA(theQuery)
             .then((result) => {
                 if (result.data.length) {
                     console.log('no result');
-                    document.getElementById('result').innerText = 'no result'
+                    document.getElementById('result').innerText = 'no result';
                 } else {
-                    const theIngredients = result.data.foods[ 0 ].food.ing.desc
-                    if (theIngredients.indexOf('SUGAR') > 0) { alert('sugar!'); };
-                    document.getElementById('result').innerText = theIngredients
+                    const theName = result.data.foods[ 0 ].food.desc.name + ' ' + result.data.foods[ 0 ].food.desc.manu;
+                    const theIngredients = result.data.foods[ 0 ].food.ing.desc;
+                    document.getElementById('result').innerHTML = '<strong>' + theName + '</strong><br />' + theIngredients
+                    let theAlertHits = [];
+                    for (let i = 0; i < theIngredients.length; i++) {
+                        if (theIngredients.indexOf(theAlerts[ i ]) > 0) {
+                            theAlertHits.push(theAlerts[ i ]);
+                        }
+                    }
+                    if (theAlertHits.length > 0) {
+                        setTimeout(function () {
+                            alert('This product contains ' + theAlertHits.join(', ').toLowerCase());
+                        }, 100);
+                    };
                 }
                 return false;
             })
             .catch(err => console.log(err));
     };
 
+    granola = () => {
+        document.getElementById('query').value = '021908498263';
+        this.queryUSDA();
+    }
+
+    bread = () => {
+        document.getElementById('query').value = '073410013755';
+        this.queryUSDA();
+    }
+
+    soup = () => {
+        document.getElementById('query').value = '00290906';
+        this.queryUSDA();
+    }
+
+    cornbread = () => {
+        document.getElementById('query').value = '072486010040';
+        this.queryUSDA();
+    }
+
     render () {
         return (
             <div>
-                <input id='query' defaultValue='00014885'></input>
-                <br />
                 <input type="file" accept="image/*" capture="camera" />
                 <br />
-                <button onClick={ this.queryUSDA }>button</button>
                 <br />
-                <div id='result'></div>
+                <strong>Barcode:</strong> <input id='query' defaultValue='00014885'></input>
+                <br />
+                <strong>Watch for:</strong> <input id='alert' defaultValue='salt'></input>
+                <br />
+                <button onClick={ this.queryUSDA }>Search</button>
+                <br />
+                <div id='result' width='100%'></div>
+                <br />
+                <br />
+                <br />
+                <strong>Examples:</strong>
+                <br />
+                <button onClick={ this.granola }>Granola</button>
+                <br />
+                <button onClick={ this.bread }>Bread</button>
+                <br />
+                <button onClick={ this.soup }>Soup</button>
+                <br />
+                <button onClick={ this.cornbread }>Cornbread</button>
+                <br />
+
             </div>
         );
     }
